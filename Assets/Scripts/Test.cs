ad571e9788f5f3c359a33bec07ce1c5b4a4e835a
rsredsq <rsredsq@gmail.com>
2017-11-29 20:57:36 +0300

    added editor conf and some code style configuration

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour {
+  // Use this for initialization
+  void Start() {
+    Debug.Log("Antoha lox");
+  }
 
-	// Use this for initialization
-	void Start () {
-		Debug.Log("Antoha lox");
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+  // Update is called once per frame
+  void Update() {
+  }
 }
