f5035c9b2a9f51a79c755b9b38ed7cf5d6020f7f
Anton Klochkov <tklochkov@gmail.com>
2017-12-06 23:56:33 +0300

    Changed scale of calculations

--- Assets/Scripts/Simulation/PhysicsConstants.cs
+++ Assets/Scripts/Simulation/PhysicsConstants.cs
@@ -2,7 +2,7 @@
 
 namespace Simulation {
   public class PhysicsConstants {
-    private const double SCALE = 1e3;
+    private const double SCALE = 1e2;
 
     public const double COULOMB_KOEF = 8.987552e9 * SCALE * SCALE;
     public const double LORENTZ_FOEF = 1e-7 / SCALE * 4 * Math.PI;
