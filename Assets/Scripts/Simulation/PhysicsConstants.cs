59d2fff42d244d9b74ac12943ad6ab03dc758798
Anton Klochkov <tklochkov@gmail.com>
2017-12-08 02:06:25 +0300

    Created new scene

--- Assets/Scripts/Simulation/PhysicsConstants.cs
+++ Assets/Scripts/Simulation/PhysicsConstants.cs
@@ -2,7 +2,7 @@
 
 namespace Simulation {
   public static class PhysicsConstants {
-    private const double SCALE = 1e3;
+    private const double SCALE = 1e4;
 
     public const double COULOMB_KOEF = 8.987552e9 * SCALE * SCALE;
     public const double LORENTZ_FOEF = 1e-7 / SCALE;
