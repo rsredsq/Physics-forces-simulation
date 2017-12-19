78d4ac8e7c42d3ae6facd5b571209ffca4e9dfb4
Anton Klochkov <tklochkov@gmail.com>
2017-12-19 21:06:18 +0300

    Changed constants

--- Assets/Scripts/Simulation/PhysicsConstants.cs
+++ Assets/Scripts/Simulation/PhysicsConstants.cs
@@ -2,11 +2,11 @@
 
 namespace Simulation {
   public static class PhysicsConstants {
-    private const float SCALE = 1e6f;
+    private const float SCALE = 1e4f;
 
     public const float COULOMB_KOEF = 8.988e9f * SCALE * SCALE;
     public const float LORENTZ_FOEF = 1e-7f * SCALE * SCALE;
 
-    public const float ACCURACY = 1e-6f;
+    public const float ACCURACY = 1e-13f;
   }
 }
