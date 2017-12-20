d2cc32193435e2b18cc02dfe4e6cc621402c536e
Anton Klochkov <tklochkov@gmail.com>
2017-12-20 13:10:31 +0300

    Added new constant

--- Assets/Scripts/Simulation/PhysicsConstants.cs
+++ Assets/Scripts/Simulation/PhysicsConstants.cs
@@ -2,13 +2,13 @@
 
 namespace Simulation {
   public static class PhysicsConstants {
-    private const float SCALE = 1e4f;
+    private const float SCALE = 1e3f;
 
     public const float COULOMB_KOEF = 8.988e9f * SCALE * SCALE;
     public const float LORENTZ_FOEF = 1e-7f * SCALE * SCALE;
 
     public const float ACCURACY = 1e-13f;
 
-    public const float DEFAULT_CHARGE = 1.5e-5f;
+    public const float DEFAULT_CHARGE = 1.5e-6f;
   }
 }
