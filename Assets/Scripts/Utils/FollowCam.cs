fe2c43d1c8445f5cc7f82f11c8786f3979ee89bf
Anton Klochkov <tklochkov@gmail.com>
2017-12-21 00:05:15 +0300

    Added follow cam

---
+++ Assets/Scripts/Utils/FollowCam.cs
@@ -0,0 +1,90 @@
+﻿using UnityEngine;
+
+namespace Utils {
+  [RequireComponent(typeof(Simulation.ChargedObject))]
+  public class FollowCam : MonoBehaviour {
+    public Transform Target;
+
+    public float SpeedX = 360f;
+    public float SpeedY = 240f;
+    public float LimitY = 70f;
+
+    public LayerMask Obstances;
+
+    private float maxDistance;
+
+    private Vector3 localPosition;
+    private float currentYRotation;
+
+    private Vector3 Position {
+      get { return transform.position; }
+      set { transform.position = value; }
+    }
+
+    private void Start() {
+      localPosition = Target.InverseTransformPoint(Position);
+      maxDistance = Vector3.Distance(Position, Target.position);
+    }
+
+    private void LateUpdate() {
+      if (!IsCusorLocked())
+        return;
+
+      Position = Target.TransformPoint(localPosition);
+      CameraRotation();
+      ObstaclesReact();
+      localPosition = Target.InverseTransformPoint(Position);
+    }
+
+    private void CameraRotation() {
+      RotationMouseY();
+      RotationMouseX();
+
+      transform.LookAt(Target);
+    }
+
+    private void RotationMouseX() {
+      var nx = Input.GetAxis("Mouse X");
+
+      if (Mathf.Approximately(nx, 0f)) {
+        return;
+      }
+
+      transform.RotateAround(Target.position, Vector3.up, nx * SpeedX * Time.deltaTime);
+    }
+
+    private void RotationMouseY() {
+      var ny = Input.GetAxis("Mouse Y");
+
+      if (Mathf.Approximately(ny, 0f)) {
+        return;
+      }
+
+      var tmp = Mathf.Clamp(currentYRotation + ny * SpeedY * Time.deltaTime, -LimitY, LimitY);
+
+      if (Mathf.Approximately(tmp, currentYRotation)) {
+        return;
+      }
+
+      var rot = tmp - currentYRotation;
+      transform.RotateAround(Target.position, transform.right, rot);
+      currentYRotation = tmp;
+    }
+
+    private void ObstaclesReact() {
+      var distance = Vector3.Distance(Position, Target.position);
+      RaycastHit hit;
+
+      if (Physics.Raycast(Target.position, transform.position - Target.position, out hit, maxDistance, Obstances)) {
+        Position = hit.point;
+      }
+      else if (distance < maxDistance && !Physics.Raycast(Position, -transform.forward, .1f, Obstances)) {
+        Position -= transform.forward * .05f;
+      }
+    }
+
+    private static bool IsCusorLocked() {
+      return Cursor.lockState != CursorLockMode.None;
+    }
+  }
+}
