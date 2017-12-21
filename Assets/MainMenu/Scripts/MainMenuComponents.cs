010c06354490130a29a3bf418af9f0975b3cd09c
Anton Klochkov <tklochkov@gmail.com>
2017-12-21 22:20:43 +0300

    Added MainMenu

---
+++ Assets/MainMenu/Scripts/MainMenuComponents.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class MainMenuComponents : MonoBehaviour {
+    public AudioSource ClickSound;
+    public AudioSource HoverSound;
+
+    public Animator CameraObject;
+
+    public void Position(int position) {
+        CameraObject.SetFloat("Animate", position);
+    }
+
+    public void PlayHover() {
+        HoverSound.Play();
+    }
+
+    public void PlayClick() {
+        ClickSound.Play();
+    }
+}
---
+++ Assets/MainMenu/Scripts/SelectOnInput.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SelectOnInput : MonoBehaviour {
+    public EventSystem EventS;
+    public GameObject SelectedObject;
+
+    private bool buttonSelected;
+
+    public void SetButtonSelected(bool state) {
+        buttonSelected = state;
+    }
+
+    public void Update() {
+        if ((Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0) && buttonSelected == false) {
+            EventS.SetSelectedGameObject(SelectedObject);
+            SetButtonSelected(true);
+        }
+    }
+
+    private void OnDisable() {
+        SetButtonSelected(false);
+    }
+}
