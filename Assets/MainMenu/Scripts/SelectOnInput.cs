6db98f5a26c45d03b460a96319c84293218666ba
Anton Klochkov <tklochkov@gmail.com>
2017-12-22 20:39:20 +0300

    Fixed bugs

--- Assets/MainMenu/Scripts/SelectOnInput.cs
+++ Assets/MainMenu/Scripts/SelectOnInput.cs
@@ -2,23 +2,25 @@
 using UnityEngine.EventSystems;
 
 public class SelectOnInput : MonoBehaviour {
-    public EventSystem EventS;
-    public GameObject SelectedObject;
+  public EventSystem EventS;
+  public GameObject SelectedObject;
+  public MainMenuComponents SoundObject;
 
-    private bool buttonSelected;
+  private bool buttonSelected;
 
-    public void SetButtonSelected(bool state) {
-        buttonSelected = state;
+  public void SetButtonSelected(bool state) {
+    buttonSelected = state;
+  }
+
+  public void Update() {
+    if ((Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0) && buttonSelected == false) {
+      EventS.SetSelectedGameObject(SelectedObject);
+      SetButtonSelected(true);
     }
+  }
 
-    public void Update() {
-        if ((Input.GetAxisRaw("Vertical") != 0 || Input.GetAxisRaw("Horizontal") != 0) && buttonSelected == false) {
-            EventS.SetSelectedGameObject(SelectedObject);
-            SetButtonSelected(true);
-        }
-    }
-
-    private void OnDisable() {
-        SetButtonSelected(false);
-    }
+  public void OnDisable() {
+    SetButtonSelected(false);
+    EventS.SetSelectedGameObject(null);
+  }
 }
